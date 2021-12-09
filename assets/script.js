4bd321b648c217d2b5e38f436d33fcc0be211519
Jonathan Chen <jontkc98@gmail.com>
2021-12-09 13:00:56 -0800

    Added fetch function for the open weather api

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,21 @@ dropdown.addEventListener('click', function(event) {
   event.stopPropagation();
   dropdown.classList.toggle('is-active');
 });
+
+function getData(input) {
+    var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=31ed1d78ece05a26dbb0c6020e7b32b5";
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                resonse.json().then(function (data) {
+                    console.log(data);
+                    return(data);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert("Did not get a resposne");
+        });
+};
