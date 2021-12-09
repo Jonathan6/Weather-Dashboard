5e1f93b5729e2f27d0a168f95a302652f1460bfc
Jonathan Chen <jontkc98@gmail.com>
2021-12-09 14:57:34 -0800

    Working on fetching weather data. Added more ids to the HTML to access tags.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,28 @@
-var dropdown = document.querySelector('.dropdown');
-dropdown.addEventListener('click', function(event) {
+var dropdownEl = $('.dropdown');
+var inputEl = $(".input");
+var buttonEl = $(".button");
+
+var weatherEl = $("#0");
+
+var data2;
+
+dropdownEl.on('click', function(event) {
   event.stopPropagation();
-  dropdown.classList.toggle('is-active');
+  dropdownEl.toggleClass('is-active');
+});
+
+buttonEl.on("click", function(event) {
+    data2 = getData(inputEl.val().trim());
+    console.log(data2);
+    // renderPage(data);
 });
 
 function getData(input) {
-    var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=31ed1d78ece05a26dbb0c6020e7b32b5";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + input + "&appid=31ed1d78ece05a26dbb0c6020e7b32b5";
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
-                resonse.json().then(function (data) {
-                    console.log(data);
+                response.json().then(function (data) {
                     return(data);
                 });
             } else {
@@ -21,3 +33,10 @@ function getData(input) {
             alert("Did not get a resposne");
         });
 };
+
+function renderPage(data) {
+    var current = data.list[0].main;
+    $("#avg").text(current.temp);
+    $("#high").text(current.temp_max);
+    $("#low").text(current.temp_low);
+}
