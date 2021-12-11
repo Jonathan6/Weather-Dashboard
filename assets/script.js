b013976397ca235341c06033e292aa587db40f46
Jonathan Chen <jontkc98@gmail.com>
2021-12-11 10:06:38 -0800

    Added api data as a accessible local variable. Added functions to load the page without sending api request everytime I load the page. Added function that organizes all data from api by date

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,9 @@ var windEl = document.getElementsByClassName("wind");
 var precipitationEl = document.getElementsByClassName("precipitation");
 
 var weatherData;
+var datedData = {};
 var searchHistory = [];
+
 
 dropdownEl.on('click', function(event) {
   event.stopPropagation();
@@ -22,6 +24,8 @@ searchEl.on("click", function(event) {
     weatherData = getData(inputEl.val().trim());
 });
 
+
+
 function getData(input) {
     recordSearch(input);
     var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + input + "&appid=31ed1d78ece05a26dbb0c6020e7b32b5";
@@ -30,8 +34,7 @@ function getData(input) {
             if (response.ok) {
                 response.json().then(function (data) {
                     renderPage(data);
-                    console.log(data);
-                    return data;
+                    weatherData = data;
                 });
             } else {
                 alert("Error: " + response.statusText);
@@ -52,7 +55,7 @@ function renderPage(data) {
         lowEl[i].textContent = "Low: " + currentTemp.temp_min + "\u00B0C";
         humidityEl[i].textContent = "Humidity: " + currentTemp.humidity + "%";
 
-        dateEl[i].textContent = current.dt_txt.substring(5,10);
+        dateEl[i].textContent = current.dt_txt.substring(0,10);
         windEl[i].textContent = "Wind: " + current.wind.speed + "MPH";
         // TODO: adding in precipitation and dealing with non guarenteed data
         // if (current.rain["3h"] !== undefined) {
@@ -71,3 +74,41 @@ function recordSearch(input) {
     localStorage.setItem("history", searchHistory);
 }
 
+function unixToDate(unix) {
+    let unix_timestamp = unix;
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(unix_timestamp * 1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+}
+
+// Organizes api weather data based on date
+function organizeData() {
+    // For loop that goes through all elements in data
+    weatherData.list.forEach(element => {
+        var currentDate = element.dt_txt.substring(0,10);
+        if (!(currentDate in datedData)) {
+            datedData[currentDate] = [];
+        }
+        datedData[currentDate].push(element);
+    });
+}
+
+function saveDataLocal() {
+    localStorage.setItem("TEST", JSON.stringify(weatherData));
+}
+
+function loadDataLocal() {
+    weatherData = JSON.parse(localStorage.getItem("TEST"));
+}
+
+loadDataLocal();
